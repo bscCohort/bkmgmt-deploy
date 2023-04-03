ccaaa147eb73402ee5fcd4da515d11bb6ca2cf4e
BscCohort Admin <bsc.cohort.io@gmail.com>
2023-04-03 04:54:16 +0000

    fixed button styling

--- client/src/components/ShowBookDetails.js
+++ client/src/components/ShowBookDetails.js
@@ -87,7 +87,7 @@ function ShowBookDetails(props) {
             <hr /> <br />
           </div>
           <div className='col-md-10 m-auto'>{BookItem}</div>
-          <div className='col-md-6 m-auto'>
+          <div className='col-md-4 m-auto'>
             <button
               type='button'
               className='btn btn-outline-danger btn-lg btn-block'
@@ -98,7 +98,7 @@ function ShowBookDetails(props) {
               Delete Book
             </button>
           </div>
-          <div className='col-md-6 m-auto'>
+          <div className='col-md-4 m-auto'>
             <Link
               to={`/edit-book/${book._id}`}
               className='btn btn-outline-info btn-lg btn-block'
